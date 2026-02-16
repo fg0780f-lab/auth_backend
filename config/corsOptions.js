c4448b1c978417a941e81b3595eae51e9d8c3f7d
abdul hassan jamal <fg0780f@gmail.com>
2026-02-15 18:45:32 -0900

    fix: some update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin:(origin , callback)=> {
-        if(allowedOrigins.includes(origin) || (!origin && process.env.NODE_ENV == "development") ){
+        if(allowedOrigins.includes(origin)){
             callback(null , true)
         }else{
             callback(new Error("Not allowed by cors"))
