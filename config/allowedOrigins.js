c33c8d61a7d25585e9f4abee1bf7fe64c8fa5106
abdul hassan jamal <fg0780f@gmail.com>
2026-02-15 18:54:00 -0900

    fix: some update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://localhost:3000"]
+const allowedOrigins = [
+    "http://localhost:3000",
+    'https://auth-backend-pudc3jkwp-fg0780f-labs-projects.vercel.app',
+]
 
 
 
